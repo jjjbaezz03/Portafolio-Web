e830ad5067f2fb911b452e1abae0c2d2e5ebd9ad
jjjbaezz03 <111792769+jjjbaezz03@users.noreply.github.com>
2022-09-14 14:52:55 -0400

    Add files via upload

---
+++ JS/Scrollreveal.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.presentation');
+ScrollReveal().reveal('.about', { delay: 500 });
+ScrollReveal().reveal('.projects', { delay: 500 });
+ScrollReveal().reveal('.testimony', { delay: 500 });
+ScrollReveal().reveal('.presentation', { delay: 500 });
+ScrollReveal().reveal('.presentation', { delay: 500 });
+
